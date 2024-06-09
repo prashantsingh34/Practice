8320b2974355ce956b7e8b29b395a9d930eb1aec
prashant <prashantsingh830@gmail.com>
2024-06-09 21:32:38 +0530

    m

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -7,7 +7,7 @@ export const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
-        <h1>Tech Shopping from here</h1>
+        <h1>Tech Shop</h1>
       </div>
 
       <div className="products">
