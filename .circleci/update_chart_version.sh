b9e3665fd5ea6ad914d0618b16f4d58d8dab3d83
prashant <prashantsingh830@gmail.com>
2024-06-09 18:46:02 +0530

    ml

--- .circleci/update_chart_version.sh
+++ .circleci/update_chart_version.sh
@@ -4,8 +4,10 @@ set -e
 
 # Variables
 HELM_REPO_URL=$1
+NEW_VERSION=$3
 EMAIL=$2
 CHART_YAML_PATH="cart-service/Chart.yaml"  # Adjust the path to the Chart.yaml file in the Helm repo
+IMAGE_YAML_PATH="cart-serviice/values.yaml"
 GIT_BRANCH="main"  # Branch to push the changes to
 # Clone the Helm chart repository 
 git clone "https://prashant:${TOKEN}@github.com/$HELM_REPO_URL.git"
@@ -13,20 +15,25 @@ git clone "https://prashant:${TOKEN}@github.com/$HELM_REPO_URL.git"
 
 cd $(basename "$HELM_REPO_URL")
 
-# Extract the current version
-CURRENT_VERSION=$(grep '^version:' "$CHART_YAML_PATH" | awk '{print $2}')
-echo "Current version: $CURRENT_VERSION"
+# # Extract the current version
+# CURRENT_VERSION=$(grep '^version:' "$CHART_YAML_PATH" | awk '{print $2}')
+# echo "Current version: $CURRENT_VERSION"
 
-# Increment the patch version (x.y.z -> x.y.(z+1))
-IFS='.' read -r -a VERSION_PARTS <<< "$CURRENT_VERSION"
-PATCH_VERSION=${VERSION_PARTS[2]}
-NEW_PATCH_VERSION=$((PATCH_VERSION + 1))
-NEW_VERSION="${VERSION_PARTS[0]}.${VERSION_PARTS[1]}.$NEW_PATCH_VERSION"
-echo "New version: $NEW_VERSION"
+# # Increment the patch version (x.y.z -> x.y.(z+1))
+# IFS='.' read -r -a VERSION_PARTS <<< "$CURRENT_VERSION"
+# PATCH_VERSION=${VERSION_PARTS[2]}
+# NEW_PATCH_VERSION=$((PATCH_VERSION + 1))
+# NEW_VERSION="${VERSION_PARTS[0]}.${VERSION_PARTS[1]}.$NEW_PATCH_VERSION"
+# echo "New version: $NEW_VERSION"
 
 # Update the version in Chart.yaml
 sed -i.bak "s/^version: .*/version: $NEW_VERSION/" "$CHART_YAML_PATH"
 rm "$CHART_YAML_PATH.bak"
+
+#Update the version in values.yaml
+# Update the tag in values.yaml
+sed -i.bak 's/tag: ".*"/tag: "'$NEW_VERSION'"/' "$IMAGE_YAML_PATH"
+rm "$IMAGE_YAML_PATH.bak"
 
 # Commit and push the changes
 git config --global user.email "prashantsingh830@gmail.com"
